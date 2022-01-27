2333bd73c4a91e0fd9cad02ae61cf7e5cb7c038d
Momofil31 <filippomomesso@gmail.com>
2022-01-27 13:22:52 +0100

    :sparkles: Update manim animation w/ moving kernel

--- hitmiss-animation/scene.py
+++ hitmiss-animation/scene.py
@@ -1,7 +1,11 @@
 from manim import *
+from matplotlib.pyplot import table
 import numpy as np
 import cv2
 import manimpango
+
+SCALE_VALUE = 0.4
+RUN_TIME = 0.8
 
 # small_blob data
 image = np.array([
@@ -21,6 +25,12 @@ kernel = np.array([
     [-1, 1, -1]
 ], dtype="int8")
 
+kernel_temp = np.array([
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0]
+], dtype="int8")
+
 
 def color_cells(table: MathTable):
     for i, row in enumerate(image, 1):
@@ -38,7 +48,7 @@ class CreateTable(Scene):
         t0 = IntegerTable(image, include_outer_lines=True)
 
         color_cells(t0)
-        t0.scale(0.4)
+        t0.scale(SCALE_VALUE)
         t0.shift(LEFT*4)
         cell_width = t0.get_cell((1, 1)).width
         cell_height = t0.get_cell((1, 1)).height
@@ -49,22 +59,33 @@ class CreateTable(Scene):
         kernel_table = IntegerTable(kernel, include_outer_lines=True)
         kernel_table.set_color(BLUE_D)
 
-        kernel_table.scale(0.4)
+        kernel_table.scale(SCALE_VALUE)
+
+        moving_kernel = IntegerTable(
+            kernel_temp, include_outer_lines=True, arrange_in_grid_config={"col_alignments": "rrr"})
+        moving_kernel.set_color(BLUE_D)
+
+        for i, e in enumerate(moving_kernel.get_entries()):
+            if i == 6 or i == 8:
+                e.set_value(-1)
+                e.shift(LEFT*0.4*cell_width)
+            e.shift(DOWN*0.5*cell_height + RIGHT*0.7*cell_width)
+            e.scale(0.7)
+
+        moving_kernel.scale(SCALE_VALUE)
 
         kernel_title = Text("Kernel", font_size=15, font="Noto Sans")
         kernel_title.next_to(kernel_table, UP)
 
-        rect = Rectangle(color=BLUE_D, height=kernel_table.height,
-                         width=cell_width*3)
-        rect_2 = Rectangle(color=BLUE_D, height=cell_height, width=cell_width)
-
         t1 = IntegerTable(np.zeros(image.shape, dtype="uint8"),
                           include_outer_lines=True)
-        t1.scale(0.4)
+        t1.scale(SCALE_VALUE)
         t1.shift(RIGHT*4)
 
         t1_title = Text("Hit-Or-Miss Output", font_size=15, font="Noto Sans")
         t1_title.next_to(t1, UP)
+
+        rect_2 = Rectangle(color=BLUE_D, height=cell_height, width=cell_width)
 
         self.add(scene_title)
         self.play(scene_title.animate.move_to(UP*3))
@@ -78,21 +99,23 @@ class CreateTable(Scene):
         self.add(t1_title)
 
         self.add(kernel_table)
-        self.play(Create(kernel_table), run_time=0.8)
+        self.play(Create(kernel_table), run_time=RUN_TIME)
         self.add(kernel_title)
 
-        self.add(rect)
-        self.play(rect.animate.align_to(t0, UL), run_time=0.8)
+        self.add(moving_kernel)
+        self.play(moving_kernel.animate.align_to(t0, UL), run_time=RUN_TIME)
+        self.add(moving_kernel)
+        self.play(moving_kernel.animate.align_to(t0, UL), run_time=RUN_TIME)
         self.add(rect_2)
         self.play(rect_2.animate.align_to(t1, UL).shift(
-            RIGHT*cell_width+DOWN*cell_height), run_time=0.8)
+            RIGHT*cell_width+DOWN*cell_height), run_time=RUN_TIME)
 
         for i, row in enumerate(image[1:-1], 2):
             for j, pixel in enumerate(row[1:-1], 2):
                 sub_image = image[i-2:i+1, j-2:j+1]
                 #print((i, j))
-                #print(sub_image)
-                #print(kernel)
+                # print(sub_image)
+                # print(kernel)
 
                 match = True
                 for r in range(0, len(kernel)):
@@ -100,7 +123,7 @@ class CreateTable(Scene):
                         if kernel[r, c] != -1 and sub_image[r, c] != kernel[r, c]:
                             match = False
                             break
-                #print(match)
+                # print(match)
                 if match:
                     t1.add_highlighted_cell(
                         (i, j), color=color.WHITE, fill_opacity=1.0)
@@ -108,15 +131,15 @@ class CreateTable(Scene):
                     e.set_color(color.BLACK)
                     e.set_value(1)
                 if j < len(row)-1:
-                    self.play(rect.animate.shift(
+                    self.play(moving_kernel.animate.shift(
                         RIGHT*cell_width), rect_2.animate.shift(
-                        RIGHT*cell_width), run_time=0.8)
+                        RIGHT*cell_width), run_time=RUN_TIME)
 
             if i < len(image)-1:
-                self.play(rect.animate.shift(
+                self.play(moving_kernel.animate.shift(
                     DOWN*cell_height).align_to(t0, LEFT), rect_2.animate.align_to(t1, LEFT).shift(
-                    DOWN*cell_height+RIGHT*cell_width), run_time=0.8)
+                    DOWN*cell_height+RIGHT*cell_width), run_time=RUN_TIME)
             else:
-                self.remove(rect, rect_2)
+                self.remove(moving_kernel, rect_2)
 
         self.wait(1)
